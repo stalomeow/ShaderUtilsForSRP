031dae5d01d6d909ae551b38f9cd54a0222023c4
stalomeow <stalowork@163.com>
2023-07-07 19:53:33 +0800

    Improve performance

--- Editor/StaloSRPShaderGUI.cs
+++ Editor/StaloSRPShaderGUI.cs
@@ -26,19 +26,22 @@ internal class StaloSRPShaderGUI : ShaderGUI
         };
     });
 
+    private List<PropertyGroup> m_PropGroups = null;
     private Dictionary<uint, AnimBool> m_ExpandStates = new();
     private SearchField m_Search = new();
     private string m_SearchText = "";
 
     public override void OnGUI(MaterialEditor editor, MaterialProperty[] properties)
     {
+        Shader shader = ((Material)editor.target).shader;
+        InitializePropertyGroupsIfNot(ref m_PropGroups, shader, properties);
+
         editor.SetDefaultGUIWidths();
 
         using (new GUILayout.VerticalScope())
         {
             // Shader Header
             GUILayout.Space(5);
-            Shader shader = ((Material)editor.target).shader;
             EditorGUILayout.LabelField(shader.name, s_ShaderHeaderLabelStyle.Value);
 
             // Search Field
@@ -50,9 +53,9 @@ internal class StaloSRPShaderGUI : ShaderGUI
             // 对齐 Slider 右侧输入框和其他输入框的宽度
             using (EditorGUIScopes.LabelWidth(EditorGUIUtility.labelWidth - 5f))
             {
-                foreach (PropertyGroup group in GroupAndFilterProperties(editor, properties, m_SearchText))
+                foreach (PropertyGroup group in m_PropGroups)
                 {
-                    group.OnGUI(editor, m_ExpandStates);
+                    group.OnGUI(editor, m_ExpandStates, m_SearchText);
                 }
             }
 
@@ -70,13 +73,14 @@ internal class StaloSRPShaderGUI : ShaderGUI
         editor.DoubleSidedGIField();
     }
 
-    private struct PropertyGroup
+    private class PropertyGroup
     {
         private readonly GUIContent m_Title;
         private readonly string m_Helps;
         private readonly uint m_BitExpanded;
         private readonly List<MaterialProperty> m_Properties;
-        private readonly List<float> m_PropertyHeights;
+
+        public PropertyGroup() : this(null, null, 0) { }
 
         public PropertyGroup(string title, string helps, uint bitExpanded)
         {
@@ -84,26 +88,43 @@ internal class StaloSRPShaderGUI : ShaderGUI
             m_Helps = helps;
             m_BitExpanded = bitExpanded;
             m_Properties = new List<MaterialProperty>();
-            m_PropertyHeights = new List<float>();
         }
 
         public bool IsDefaultGroup => string.IsNullOrWhiteSpace(m_Title.text) || (m_BitExpanded == 0);
 
         public int PropertyCount => m_Properties.Count;
 
-        public void AddProperty(MaterialProperty property, float height)
+        public bool TryAddProperty(MaterialProperty property)
         {
+            if ((property.flags & MaterialProperty.PropFlags.HideInInspector) != 0)
+            {
+                // 属性被隐藏
+                return false;
+            }
+
             m_Properties.Add(property);
-            m_PropertyHeights.Add(height);
+            return true;
         }
 
-        public void OnGUI(MaterialEditor editor, IDictionary<uint, AnimBool> expandStates)
+        public bool OnGUI(
+            MaterialEditor editor,
+            IDictionary<uint, AnimBool> expandStates,
+            string searchText,
+            StringComparison searchComparisonType = StringComparison.InvariantCultureIgnoreCase)
         {
+            Action propDrawer = CreateFilteredPropertyDrawer(editor, searchText, searchComparisonType);
+
+            if (propDrawer == null)
+            {
+                // 没有属性需要绘制
+                return false;
+            }
+
             if (IsDefaultGroup)
             {
-                DrawProperties(editor);
+                propDrawer();
                 GUILayout.Space(10);
-                return;
+                return true;
             }
 
             using (MaterialHeaderScope headerScope = new(m_Title, m_BitExpanded, editor, spaceAtEnd: false))
@@ -131,85 +152,83 @@ internal class StaloSRPShaderGUI : ShaderGUI
                         GUILayout.Space(5);
                     }
 
-                    DrawProperties(editor);
+                    propDrawer();
                     GUILayout.Space(10);
                 }
 
                 EditorGUILayout.EndFadeGroup();
             }
+
+            return true;
         }
 
-        private void DrawProperties(MaterialEditor editor)
+        private Action CreateFilteredPropertyDrawer(
+            MaterialEditor editor,
+            string searchText,
+            StringComparison searchComparisonType)
         {
-            for (int i = 0; i < m_Properties.Count; i++)
+            bool enableSearch = !string.IsNullOrWhiteSpace(searchText);
+            List<(MaterialProperty prop, float height)> validProps = new();
+
+            foreach (MaterialProperty prop in m_Properties)
             {
-                MaterialProperty prop = m_Properties[i];
-                float height = m_PropertyHeights[i];
+                float height = editor.GetPropertyHeight(prop, prop.displayName);
 
-                Rect rect = EditorGUILayout.GetControlRect(true, height, EditorStyles.layerMaskField);
-                editor.ShaderProperty(rect, prop, prop.displayName);
+                if (height <= 0)
+                {
+                    // 属性没有高度
+                    // 由于 GetControlRect 返回的 rect 的高度有一个最小值，所以需要提前剔除高度为 0 的属性
+                    continue;
+                }
+
+                if (enableSearch && !prop.displayName.Contains(searchText, searchComparisonType))
+                {
+                    // 属性不符合搜索
+                    continue;
+                }
+
+                validProps.Add((prop, height));
             }
-        }
 
-        public static PropertyGroup CreateDefault() => new(null, null, 0);
+            return validProps.Count == 0 ? null : () =>
+            {
+                foreach ((MaterialProperty prop, float height) in validProps)
+                {
+                    Rect rect = EditorGUILayout.GetControlRect(true, height, EditorStyles.layerMaskField);
+                    editor.ShaderProperty(rect, prop, prop.displayName);
+                }
+            };
+        }
     }
 
     public static readonly string HeaderFoldoutAttrName = "HeaderFoldout";
 
-    private static List<PropertyGroup> GroupAndFilterProperties(
-        MaterialEditor editor,
-        MaterialProperty[] properties,
-        string searchText,
-        StringComparison searchComparisonType = StringComparison.InvariantCultureIgnoreCase)
+    private static void InitializePropertyGroupsIfNot(
+        ref List<PropertyGroup> groups,
+        Shader shader,
+        MaterialProperty[] properties)
     {
-        List<PropertyGroup> groups = new();
-        PropertyGroup currentGroup = PropertyGroup.CreateDefault();
+        if (groups != null)
+        {
+            return;
+        }
 
-        Shader shader = ((Material)editor.target).shader;
-        bool enableSearch = !string.IsNullOrWhiteSpace(searchText);
+        groups = new List<PropertyGroup>() { new PropertyGroup() }; // With One Default Group
 
         for (var i = 0; i < properties.Length; i++)
         {
-            // 创建新的组
             if (TryGetHeaderFoldoutAttribute(shader, i, out string headerTitle, out string headerHelps))
             {
-                groups.Add(currentGroup);
-
+                // 创建新的组
                 uint bitExpanded = 1u << (groups.Count - 1);
-                currentGroup = new PropertyGroup(headerTitle, headerHelps, bitExpanded);
+                groups.Add(new PropertyGroup(headerTitle, headerHelps, bitExpanded));
             }
 
-            MaterialProperty prop = properties[i];
-
-            // 被隐藏的属性
-            if ((prop.flags & MaterialProperty.PropFlags.HideInInspector) != 0)
-            {
-                continue;
-            }
-
-            // 不符合搜索的属性
-            if (enableSearch && !prop.displayName.Contains(searchText, searchComparisonType))
-            {
-                continue;
-            }
-
-            float height = editor.GetPropertyHeight(prop, prop.displayName);
-
-            // 没有高度的属性
-            // 由于 GetControlRect 返回的 rect 的高度有一个最小值，所以需要提前剔除高度为 0 的属性
-            if (height <= 0)
-            {
-                continue;
-            }
-
-            currentGroup.AddProperty(prop, height);
+            groups[^1].TryAddProperty(properties[i]);
         }
-
-        groups.Add(currentGroup);
 
         // 必须在最后移除空的组，保证前面 bitExpanded 的正确性
         groups.RemoveAll(g => g.PropertyCount == 0);
-        return groups;
     }
 
     private static bool TryGetHeaderFoldoutAttribute(
